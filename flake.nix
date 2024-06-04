a4b07a485ac234d7a856820a30c659d4eec2f857
Thomas Churchman <thomas@kepow.org>
2024-06-04 10:27:11 +0200

    build: add Nix flake development environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Map between color spaces";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              cargo
+              clippy
+              rust-analyzer
+              rustc
+              rustfmt
+            ];
+          };
+      }
+    );
+}
